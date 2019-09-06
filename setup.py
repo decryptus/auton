5eea43f420c1c0942b3f62dd4a78e9a7bd0aa5a8
Adrien Delle Cave <adrien.delle.cave@commandersact.com>
2019-09-06 17:11:18 +0200

    Fixed auton and autond arguments.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import find_packages, setup
 
-version                = '0.1.52'
+version                = '0.1.53'
 
 current_dir            = os.path.abspath(os.path.dirname(__file__))
 
